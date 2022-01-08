73fbbc74aaf0435cfb1d032fc9947f98bd3a7f4d
aicoconfs <aicotsu@gmail.com>
2022-01-09 01:02:53 +0800

    根據日期動態生成detail內的資訊

--- js/date.js
+++ js/date.js
@@ -1,3 +1,4 @@
+import './trade.js'
 // 確認檔案是否存在
 function fileExists(url){
     var isExists;
@@ -21,7 +22,7 @@ function fileExists(url){
 }
 
    
-
+// 跳過周末
 function checkday(dateTime, num=-1){
     while (!(dateTime.getDay()>0 && dateTime.getDay()<6)) {
         let date = dateTime.setDate(dateTime.getDate()+num);
@@ -29,73 +30,83 @@ function checkday(dateTime, num=-1){
         }
         return dateTime.toLocaleDateString().split('/');
         }
+// 更新前端
 function updateDate(date){
     // ex：date = [2021,2,3];
     // 更新星期幾
     let dateTime = new Date(date);
-    day = ['日', '一', '二', '三', '四', '五', '六'][dateTime.getDay()];
+    let day = ['日', '一', '二', '三', '四', '五', '六'][dateTime.getDay()];
+    let year = date[0];
     // 補零以符合格式
     date[1] = date[1].padStart(2,'0');
     date[2] = date[2].padStart(2,'0');
     $("#datetxt").text('  日期：' + date.join('/') +'  (週' + day+')');
-    // 更新圖片
-    $("#img").attr("src" , "../img/"+date.join('')+".jpg");
-    if(!(fileExists("../img/"+date.join('')+".jpg"))){
+    // 更新圖片   
+    if(fileExists("../img/"+date.join('')+".jpg")){      
+        $("#masa_msg").attr("style" , "background-image: url('');");
+        $("#img").attr("src" , "../img/"+date.join('')+".jpg");
+    }else{
         alertify.alert("沒有這天的檢討單");
+        $("#img").attr("src" , "");
+        $("#masa_msg").attr("style" , "background-image: url('../img/logo.png');");
     }
     // 更新input
     $("#date").attr("value" , date.join('-'));
     // 更新input(視覺上)
     $("#date").val(date.join('-'));
+    // 更新基本資訊
+    let jsondata = getRequest(year,date[1]+date[2])
+    let all_title=document.querySelectorAll('.title h3');
+    all_title[0].innerHTML +=jsondata['min-block'] + ' 點';
+    all_title[1].innerHTML +=jsondata['trend'] ? '趨勢':'盤整';
+    all_title[2].innerHTML +=jsondata['jump-block'][0] ? '往上開 '+jsondata['jump-block'][1]+' 點 ':'往下開 '+jsondata['jump-block'][1]+' 點 ';
+
+    // 更新檢討單
+    let table_html=document.getElementById('tablepage');
+    table_html.innerHTML = '';//reset
+    updateTable(jsondata);
+
 
 }
+// 按鈕事件
 function BtnupdateDate(num){
     // 日期增減
     let date = document.getElementById('date').getAttribute('value');
     let dateTime = new Date(date);
-    date = dateTime.setDate(dateTime.getDate()+num);
-    dateTime = new Date(date);
+    // 日期調整
+    dateTime.setDate(dateTime.getDate()+num);
     // 跳過週末的函式
     date = checkday(dateTime, num);
 
     updateDate(date);
 
 }
-// 首次更新日期
 
+// 刷新網頁時，更新成離今天最近的工作日日期
 $(document).ready(function(){
     // let date = document.getElementById('date').getAttribute('value').split('-');
     // 跳過週末的函式
-    date = checkday(new Date());
+    let date = checkday(new Date());
     updateDate(date);
     }
 );
 
 // 按鈕更新日期
 $('#back').click(function()
-{
-    BtnupdateDate(-1);
-}
+    {
+        BtnupdateDate(-1);
+    }
        
 );
 $('#next').click(function()
-{
-    BtnupdateDate(1);
-}
-       
-);        
-
-// Firefox, Google Chrome, Opera, Safari, Internet Explorer from version 9
-function OnInput (event) {
-
-    let  date =  event.target.value.split('-');
-    updateDate(date);
+    {
+        BtnupdateDate(1);
     }
-    // Internet Explorer
-    function OnPropChanged (event) {
-    if (event.propertyName.toLowerCase () == "value") {
-    let  date =  event.srcElement.value.split('-');
-    updateDate(date);
-        }
-}
+       
+);   
 
+$('#date').on('input', function(e) {
+    let date = $('#date').val().split('-');
+    updateDate(date);
+    // console.log(e.target);//不要刪掉!這值得學習
+  });
--- js/trade.js
+++ js/trade.js
@@ -1,1 +1,90 @@
 // toggle.classList.add('active');
+
+// AJAX
+// const endpoint = 'https://api.kcg.gov.tw/api/service/get/93da9fbe-2b80-439a-aec7-276366f7bfea'; 
+const endpoint = '../tradingdata/history.json'; 
+const request = new XMLHttpRequest();
+let all_data;
+request.open('GET', endpoint,true);
+request.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+request.send(null); 
+request.addEventListener('load', getRequest); 
+function getRequest(year=null , date=null) {
+    // ex：str:year=2022, str:date=0107
+    // 可以將註解拿掉查看
+    //console.log(request);
+    all_data = JSON.parse(request.responseText);
+    // for (var i in obj) {
+    //     result += obj_name + "." + i + " = " + obj[i] + "<br>";
+    //   }
+    // [1, 2, 3].forEach(function(x) {
+    //     sum += x;
+    //   });
+    if (year && date){
+        return all_data[year][date];
+    }
+    
+}
+
+function updateTable(day_data){
+    let go ;
+    let out ;
+    let table_html=document.getElementById('tablepage');
+    let result = '' ;
+    let order;
+
+    for(let i=0 ; i <day_data['trade'].length ; i++){
+        go = [
+            '<td>開盤法</td>',
+            '<td>力道歸還法</td>',
+            '<td>純訊號後回測</td>',
+            '<td>區間突破法</td>',
+            '<td>基本型態法</td>',
+            '<td>反向轉勢單</td>',
+            '<td>續走單</td>',];
+            out = [ 
+                '<td>樂高停利法</td>',
+                '<td>力道歸還法</td>',
+                '<td>強勢停利法</td>',
+                '<td>中線停利法</td>',
+                '<td>基本型態法</td>',
+                '<td>6點流</td>',
+                '<td>3點保本法</td>',];
+        result += '<h4>第'+(i+1)+'單</h4>\
+                    \n\t<div class="kind">\
+                        \n\t<table>\n\t';
+        if (day_data['trade'][i][0] == 'long'){
+            order = [['long', '多'],['short', '空']];
+        }
+        else{
+            order = [['short', '空'],['long', '多']];
+        }
+        // 進出單是哪種方式
+        let go_numlist = day_data['trade'][i][1];
+        let out_numlist = day_data['trade'][i][2];
+        // 進單
+        result += '<tr class='+order[0][0]+'>\n\t';
+        result +='<td>'+order[0][1]+'</td>\n\t';
+        go_numlist.forEach(function(go_num) {
+                go[go_num] = go[go_num].replace(/<.*?td.*?>/,'<td class="active">');//選擇特定方式active
+              });
+        
+        result +=go.join('\n\t');
+        result +='\n\t</tr>\n\t';
+        // 補單
+        result += '<tr class='+order[1][0]+'>\n\t';
+        result +='<td>'+order[1][1]+'</td>\n\t';
+        out_numlist.forEach(function(out_num) {
+            out[out_num] = out[out_num].replace(/<.*?td.*?>/,'<td class="active">');
+          });
+        result +=out.join('\n\t');
+        result +='\n\t</tr>\n\t';
+
+        result +='</table>\n\t</div>'; 
+        // 這次的單子結束
+
+        }
+    // 所有的單子結束
+    table_html.innerHTML = result;
+
+}
