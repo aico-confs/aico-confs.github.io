dbdd561baad989e2b1459fa832e7435c71b99ff7
aicoconfs <aicotsu@gmail.com>
2022-01-09 23:38:20 +0800

    追加檢討單圖片1/6~1/8

---
+++ js/bigimg.js
@@ -0,0 +1,49 @@
+
+// document.querySelector(".pic").onclick = function(){
+//         big_img.src = document.querySelector(".pic"+" img").src;
+//         big.style.display = "block";
+// }
+function fileExists(url){
+    var isExists;
+    $.ajax({
+        url:url,
+        async:false,
+        type:'HEAD',
+        error:function(){
+            isExists=0;
+        },
+        success:function(){
+            isExists=1;
+        }
+    });
+    if(isExists==1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+let big = document.querySelector(".big")
+let big_img = document.querySelector(".big .img img")
+big.onclick = function(){
+    big.style.display = "none";
+    big_img.src = "";
+}
+
+
+
+$('.pic').click(function()
+    {
+
+    let date = document.getElementById('date').getAttribute('value').split('-');
+    date[1] = date[1].padStart(2,'0');
+    date[2] = date[2].padStart(2,'0');    
+    if(fileExists("../img/"+date.join('')+".jpg")){      
+        big_img.src = document.querySelector(".pic"+" img").src;
+        big.style.display = "block";
+        }
+    }
+       
+);
